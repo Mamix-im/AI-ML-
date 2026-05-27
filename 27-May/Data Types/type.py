e387930112382c50f6dc47c4360a2c609218482e
Manav Deshmukh <manavd108@gmail.com>
2026-05-28 00:17:38 +0530

    add

---
+++ 27-May/Data Types/type.py
@@ -0,0 +1,14 @@
+# Types 
+
+M = 12/3
+A = 12 + 3j
+V = "Manav"
+print(type(M))
+print(type(A))
+print(type(V))
+print(A)
+
+# boolean 
+
+M = True
+print(type(M))
---
+++ 27-May/Type converison/cn.py
@@ -0,0 +1,18 @@
+# cheak Unicode 
+
+
+a = "M"
+print(ord(a))
+
+# indexing 
+
+A = "COLLAGE"
+
+print(A[-1])
+
+# String Slicing
+# Start, stop,step
+
+K = "WonderFulK"
+print(K[0:-1:1])
+
