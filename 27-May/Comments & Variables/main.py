25f6b053a0d3fe214eef0d7c7ed2e8895e63a6ac
Manav Deshmukh <manavd108@gmail.com>
2026-05-27 14:37:34 +0530

    First commit

---
+++ 27-May/Comments & Variables/main.py
@@ -0,0 +1,30 @@
+# CMT's
+
+"""  
+All lins is CMts
+"""
+# --> it's CMT
+
+# Variable's
+
+Name = "Manav Deshmukh"
+age = 22 
+
+#  you cant write Variable's in python using 3 Way.
+
+# Camel case 
+
+pythonCode = 22
+
+#Pascal case 
+
+PythonCode = 33
+
+# Snake case  
+
+Python_code = 44
+
+
+
+
+
