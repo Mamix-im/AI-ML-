b702f0af49afb7614e72f72443482019f484b81e
Manav Deshmukh <manavd108@gmail.com>
2026-05-28 15:15:09 +0530

    add input

---
+++ 28-May/Input,output&operators/input/out.py
@@ -0,0 +1,8 @@
+Name = "Manav"
+age = 22
+print(f"hi my name is {Name} and my age is {age}")   # this is a f-string
+
+print ("hi my name is",Name,"and my age is",age  )  # multiple values
+
+a = input("what is your age : ")
+print(f"hello your age is {a}")
